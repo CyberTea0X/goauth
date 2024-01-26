39c6d89e818d42eff7ac72a41879eef9b85ec174
CyberTea0X <mihailusov_778@mail.ru>
2024-01-26 08:25:13 +0300

    Swagger intergration started

--- controllers/healthcheck.go
+++ controllers/healthcheck.go
@@ -5,6 +5,15 @@ import (
 	"net/http"
 )
 
+// @BasePath /api
+
+// HealthCheck godoc
+// @Summary checks if server is running
+// @Schemes
+// @Description to healthcheck
+// @Tags healthcheck
+// @Success 200 {string} Server is alive!
+// @Router /health_check [get]
 func (p *PublicController) HealthCheck(c *gin.Context) {
 	c.String(http.StatusOK, "Server is alive!")
 }
