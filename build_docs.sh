8e08bd5119d66ff42154b367786faa0c23eaff56
CyberTea0X <mihailusov_778@mail.ru>
2024-02-24 22:18:30 +0300

    Added external server example docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,1 +1,4 @@
-npx --yes @redocly/cli build-docs -o docs/openapi.html docs/openapi.yaml
+npx --yes @redocly/cli build-docs -o docs/openapi.html docs/openapi.yaml &
+npx --yes @redocly/cli build-docs -o docs/external.html docs/external.yaml &
+wait
+echo "Docs are be built"
